e1eb86785147093d0d270eedfd9879a11f2c3df3
Cagdas Erman AFACAN <cagdas.afacan@hepsiburada.com>
2015-05-25 23:17:35 +0300

    server created via the given contracts path seperated by comma

---
+++ test/apiMockerSpec.js
@@ -0,0 +1,18 @@
+var fs = require("fs");
+var expect = require("chai").expect;
+var Valter = require("../lib/apiMocker.js");
+var path = require("path");
+var sinon = require("sinon");
+
+
+describe("ApiMocker", function() {
+    
+    var apiMocker= new ApiMocker()
+
+    beforeEach(function() {
+    });
+
+    it('should instantiated properly', function(){
+        
+    });
+});
