bfdc75cf4870bff283eaff9c7b5b282fab668a99
Onat Yigit Mercan <onatmercan@gmail.com>
2015-06-10 12:00:11 +0300

    adds method type support

--- app.js
+++ app.js
@@ -3,83 +3,79 @@
 var program = require('commander');
 var ApiMocker = require("./lib/api-mocker.js");
 var sys = require("sys"),
-  http = require("http"),
-  url = require("url"),
-  path = require("path"),
-  fs = require("fs"),
-  q = require("q");
+    http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs"),
+    q = require("q");
 
 program
-  .version('0.0.1')
-  .option('-p, --filepath [value]', 'contracts path')
-  .parse(process.argv);
+    .version('0.0.1')
+    .option('-p, --filepath [value]', 'contracts path')
+    .parse(process.argv);
 
-
-if (program.filepath === undefined) { throw "filepath argument is mandatory for help -h";  }
+if (program.filepath === undefined) {
+    throw "filepath argument is mandatory for help -h";
+}
 
 var splitedFilePaths = program.filepath.split(',');
 
 var contractList = [];
 
-function getContracts(){
+function getContracts() {
     var pathIndex = 0;
-    var index =0;
+    var index = 0;
     var files = [];
     console.log(splitedFilePaths);
     // loop on directories
     getContract(splitedFilePaths[pathIndex], index, pathIndex, files);
-   
+
     var apiMocker = new ApiMocker();
     apiMocker.init(contractList);
     console.log('api-mocker started for the metada files under \'%s\'\n\n', program.filepath);
 }
 
-function getContract(fp, index, pathIndex, files){
-/*
-      console.log("pathIndex:"+pathIndex);
-      console.log("index:"+index);
-      console.log("contract: "+fp);*/
+function getContract(fp, index, pathIndex, files) {
+    /*
+          console.log("pathIndex:"+pathIndex);
+          console.log("index:"+index);
+          console.log("contract: "+fp);*/
 
-      if(splitedFilePaths.length > pathIndex && fp != undefined) {
+    if (splitedFilePaths.length > pathIndex && fp != undefined) {
 
-            if(index == 0 && files.length == 0){
-              files = fs.readdirSync(splitedFilePaths[pathIndex]);
+        if (index == 0 && files.length == 0) {
+            files = fs.readdirSync(splitedFilePaths[pathIndex]);
+        }
+
+        //console.log("files.length: " + files.length);
+        if (files.length > index) {
+            // loop on files
+            /*console.log("" + splitedFilePaths[pathIndex] + files[index]);
+            console.log("has contract : " + files[index].indexOf('.contract') > -1);
+            console.log(files[index]);*/
+
+            if (files[index].indexOf('.contract') > -1) {
+                //console.log("reading : "+ splitedFilePaths[pathIndex] + files[index])
+                var serviceStub = JSON.parse(fs.readFileSync(splitedFilePaths[pathIndex] + files[index], 'utf8'));
+
+                contractList.push({
+                    method: serviceStub.on,
+                    route: serviceStub.uri,
+                    statusCode: serviceStub.expect.request.status_code,
+                    body: serviceStub.expect.body
+                });
             }
-
-            //console.log("files.length: " + files.length);
-            if(files.length > index) {
-                // loop on files
-                /*console.log("" + splitedFilePaths[pathIndex] + files[index]);
-                console.log("has contract : " + files[index].indexOf('.contract') > -1);
-                console.log(files[index]);*/
-
-                if (files[index].indexOf('.contract') > -1) {
-                    //console.log("reading : "+ splitedFilePaths[pathIndex] + files[index])
-                    var serviceStub = JSON.parse(fs.readFileSync(splitedFilePaths[pathIndex] + files[index], 'utf8'));
-                      
-                    contractList.push({
-                        route: serviceStub.uri,
-                        statusCode: serviceStub.expect.request.status_code,
-                        body: serviceStub.expect.body
-                    });
-                }
-                index++;
-                //console.log("next file: "+files[index]);
-                getContract(files[index], index, pathIndex, files);
-              }
+            index++;
+            //console.log("next file: "+files[index]);
+            getContract(files[index], index, pathIndex, files);
         }
-       else if(splitedFilePaths.length > pathIndex){
-            
-            index = 0;
-            pathIndex++;
-            files = [];
-            getContract(splitedFilePaths[pathIndex], index, pathIndex, files);
-        }
+    } else if (splitedFilePaths.length > pathIndex) {
+
+        index = 0;
+        pathIndex++;
+        files = [];
+        getContract(splitedFilePaths[pathIndex], index, pathIndex, files);
+    }
 }
 
-
 getContracts();
-
-
-
-
--- test/apiMockerSpec.js
+++ test/apiMockerSpec.js
@@ -4,15 +4,13 @@ var Valter = require("../lib/apiMocker.js");
 var path = require("path");
 var sinon = require("sinon");
 
-
 describe("ApiMocker", function() {
-    
-    var apiMocker= new ApiMocker()
 
-    beforeEach(function() {
-    });
+    var apiMocker = new ApiMocker()
 
-    it('should instantiated properly', function(){
-        
+    beforeEach(function() {});
+
+    it('should instantiated properly', function() {
+
     });
 });
